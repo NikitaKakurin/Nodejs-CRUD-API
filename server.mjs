f3c82cbb82af711e4c034d19be575fc0c3f3ef63
NikitaKakurin <neyasut87@rambler.ru>
2022-06-19 15:51:48 +0300

    fix: get server.js

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import { Console } from 'console';
 import http from 'http';
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid'
 
 
 const db = [{"id": "io"},{"id": "i"},{"id": "qw"}];
@@ -7,6 +8,10 @@ const db = [{"id": "io"},{"id": "i"},{"id": "qw"}];
 const server = http.createServer((req, res) => {
   switch (req.method) {
     case "GET":
+      get(req, res)
+    break
+
+    case "POST":
       get(req, res)
     break
     default:
@@ -21,45 +26,45 @@ const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
-async function get (request, response) {
+function get (request, response) {
   console.log(request.url)
   if (request.url === "/api/user" || request.url === "/api/user/") {
-    sendCode200(db)
+    sendResponse(response, 200, db)
     return;
   }
   if(request.url.startsWith("/api/user/")){
     const reqParts = request.url.split('/');
 
     if(reqParts.length !== 4){
-      sendCode400(`wrong request: ${request.url}`);
+      sendResponse(response, 400, `wrong request: ${request.url}`);
       return;
     };
 
     const reqId = reqParts[3].toString();
+    if(!uuidValidate(reqId)){
+      sendResponse(response, 400, `userId ${reqId} is invalid (not uuid)`);
+      return;
+    };
     let resObj = db.find((user) => user.id===reqId);
 
     if(!resObj){
-      sendCode400(`user with id: ${reqId} - not found`)
+      sendResponse(response, 404, `user with id: ${reqId} - not found`)
       return;
     };
 
-    sendCode200(resObj)
+    sendResponse(response, 200, resObj)
     return;
   }
-  sendCode400(req);
-
-  function sendCode400(message){
-    response.statusCode = 400
-    response.setHeader("Content-Type", "application/json")
-    response.write(JSON.stringify(message))
-    response.end()
-  }
-
-  function sendCode200(message){
-    response.statusCode = 400
-    response.setHeader("Content-Type", "application/json")
-    response.write(JSON.stringify(message))
-    response.end()
-  }
+  sendResponse(response, 400,`wrong request: ${request.url}`);
 }
 
+function sendResponse(response, code, message){
+  response.statusCode = code;
+  response.setHeader("Content-Type", "application/json");
+  response.write(JSON.stringify(message));
+  response.end();
+};
+
+function post(request, response){
+  
+}
